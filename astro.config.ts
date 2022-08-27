89d320defa04e3c561bda88f34d76c55c8f33538
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2022-08-27 09:55:25 +0000

    implement project page

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
+import prefetch from '@astrojs/prefetch'
 import Icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://sngr.studio',
+  integrations: [react(), prefetch()],
   vite: {
-    plugins: [Icons({ compiler: 'jsx', jsx: 'react', autoInstall: true })],
+    plugins: [
+      Icons({
+        compiler: 'jsx',
+        jsx: 'react',
+        autoInstall: true,
+      }),
+    ],
   },
 })
---
+++ src/pages/projects/suraumadani.md
@@ -0,0 +1,16 @@
+---
+title: Surau Ka'bah Madani Lamandau
+startDate: 2021-10-11
+engine: astro
+features:
+  - static site
+  - landing page
+  - donation link
+  - advanced seo
+featured: true
+draft: true
+link: 'https://www.suraumadani.com'
+layout: '~/layouts/projects.astro'
+---
+
+Surau Ka'bah Madani Lamandau adalah sebuah gerakan sosial oleh Ustadz Ahmad Zailani, di desa Bukit Indah, Kab. Lamandau, Kalimantan Tengah yang berfokus kepada pembinaan agama kepada masyarakat lokal di desa tersebut.
