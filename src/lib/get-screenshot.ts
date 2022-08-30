cde9162e05d7135db890e12419a46b0c41871f55
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2022-08-30 07:22:24 +0000

    edit api route

--- src/lib/get-screenshot.ts
+++ src/lib/get-screenshot.ts
@@ -4,7 +4,7 @@ export const getScreenshotUrl = (url: string, w?: number, h?: number) =>
     .join('/')
 
 export const getScreenshotUrlWQuery = (url: string, w?: number, h?: number) =>
-  '/screenshot/img?' +
+  '/screenshot?' +
   [w && `w=${w}w`, h && `h=${h}h`, `url=${url}`].filter(Boolean).join('&')
 
 const getScreenshot = async (src: string) => {
