af1b242d5e6d8b3f0268d284a33f185206b7b628
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2022-09-02 04:36:44 +0000

    modify hero image to link to particular project

--- src/components/react/screenshot/image.tsx
+++ src/components/react/screenshot/image.tsx
@@ -1,19 +1,36 @@
-import type { FC } from 'react'
+import type { FC, PropsWithChildren } from 'react'
 import { Loading, Error } from './state'
 import { motion } from 'framer-motion'
 import useSWR from 'swr'
 import getScreenshot, { getScreenshotUrl } from '~/lib/get-screenshot'
 import style from './screenshot.module.scss'
 
-export interface IHeroImage {
+export interface IHeroImageWrapper {
+  link?: string
+}
+
+export interface IHeroImage extends IHeroImageWrapper {
   image: string
 }
 
-const Image: FC<IHeroImage> = ({ image }) => {
-  const { data, error } = useSWR(getScreenshotUrl(image), getScreenshot)
+const ImageWrapper: FC<PropsWithChildren<IHeroImageWrapper>> = ({
+  link,
+  children,
+}) => {
+  if (link)
+    return (
+      <motion.a
+        href={link}
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.9 }}
+        className={style.__window}
+        key="heroImage"
+      >
+        {children}
+      </motion.a>
+    )
 
-  if (!data) return <Loading />
-  if (error) return <Error />
   return (
     <motion.div
       initial={{ opacity: 0, scale: 0.9 }}
@@ -22,8 +39,20 @@ const Image: FC<IHeroImage> = ({ image }) => {
       className={style.__window}
       key="heroImage"
     >
-      <img className="w-full" src={data} width={463} height={260} alt="" />
+      {children}
     </motion.div>
+  )
+}
+
+const Image: FC<IHeroImage> = ({ image, link }) => {
+  const { data, error } = useSWR(getScreenshotUrl(image), getScreenshot)
+
+  if (!data) return <Loading />
+  if (error) return <Error />
+  return (
+    <ImageWrapper link={link}>
+      <img className="w-full" src={data} width={463} height={260} alt="" />
+    </ImageWrapper>
   )
 }
 
--- src/components/react/screenshot/index.tsx
+++ src/components/react/screenshot/index.tsx
@@ -3,9 +3,9 @@ import type { IHeroImage } from './image'
 import ImageComp from './image'
 import { AnimatePresence } from 'framer-motion'
 
-const Image: FC<IHeroImage> = ({ image }) => (
+const Image: FC<IHeroImage> = ({ image, link }) => (
   <AnimatePresence mode="wait">
-    <ImageComp image={image} />
+    <ImageComp image={image} link={link} />
   </AnimatePresence>
 )
 
