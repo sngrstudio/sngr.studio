fe864262fc3a3f453440d9201858f8630615ea24
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2022-09-08 15:07:04 +0000

    implement header

---
+++ src/components/common/header/header.tsx
@@ -0,0 +1,38 @@
+import type { FC } from 'react'
+import site from '~/components/site/site'
+import menu from '~/components/site/menu'
+import MenuIcon from '~icons/mdi/menu'
+import style from './header.module.scss'
+
+const Header: FC = () => {
+  return (
+    <header className={style.__header}>
+      <nav className="navbar w-full">
+        <div className="flex-1">
+          <a href="/" rel="prefetch">
+            <site.logo.black className="w-max h-[36px]" />
+          </a>
+        </div>
+        <div className="flex-none hidden md:block">
+          <ul className="menu menu-horizontal">
+            {menu.map((item, i) => (
+              <li key={`menu-${i}`}>
+                <a href={item.link} rel="prefetch">
+                  <item.icon />
+                  <span>{item.title}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex-none md:hidden">
+          <label className="btn btn-square btn-ghost" htmlFor="main-menu">
+            <MenuIcon />
+          </label>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/site/menu.tsx
@@ -0,0 +1,23 @@
+import Trophy from '~icons/mdi/trophy'
+import Check from '~icons/mdi/clipboard-check'
+import Chat from '~icons/mdi/chat'
+
+const menu = [
+  {
+    title: 'Proyek',
+    link: '/projects',
+    icon: Trophy,
+  },
+  {
+    title: 'Layanan',
+    link: '/services',
+    icon: Check,
+  },
+  {
+    title: 'Hubungi Kami',
+    link: '/contact',
+    icon: Chat,
+  },
+]
+
+export default menu
